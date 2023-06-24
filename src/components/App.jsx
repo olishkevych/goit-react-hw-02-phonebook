eb2188297cde2b72bc94e5cd2332feea6c52270c
olishkevych <olishkevych.natalia@gmail.com>
2023-06-24 22:40:11 +0200

    full task 2 upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Component } from 'react';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import styles from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const contactToAdd = {
+      name: name,
+      number: Number(number),
+      key: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contactToAdd],
+    }));
+  };
+
+  onFilterInput = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  removeContact = key => {
+    const updatedContacts = this.state.contacts.filter(
+      contact => contact.key !== key
+    );
+    this.setState({
+      contacts: updatedContacts,
+    });
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(
+      contact =>
+        contact.name &&
+        contact.name.toLowerCase().startsWith(this.state.filter.toLowerCase())
+    );
+    return (
+      <div className={styles.wrapper}>
+        <h1>Phonebook</h1>
+        <ContactForm
+          contacts={this.state.contacts}
+          addContact={this.addContact}
+        />
+        <h2>Contacts</h2>
+        <Filter onFilterInput={this.onFilterInput} />
+        <ContactList
+          contacts={filteredContacts}
+          removeContact={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
